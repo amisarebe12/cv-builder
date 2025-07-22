20f44c33da86f615a30814713006fed92ceb9285
amisarebe12 <amisarebe12@example.com>
2025-07-22 18:38:20 +0700

    Fix TypeScript error: Handle undefined endDate in TemplateCreative projects

--- components/templates/TemplateCreative.tsx
+++ components/templates/TemplateCreative.tsx
@@ -281,7 +281,7 @@ class CreativeTemplate extends CVTemplateBase {
                       )}
                       <div className="text-xs text-gray-500 flex items-center">
                         <Calendar className="w-3 h-3 mr-1" />
-                        {this.formatDateRange(project.startDate, project.endDate)}
+                        {this.formatDateRange(project.startDate, project.endDate || 'Present')}
                       </div>
                     </div>
                   ))}
